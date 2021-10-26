fd61d0069f085060c6906e8e18030bac0c4c0f19
Yun Sung Choi <cyss0317@gmail.com>
2021-10-26 13:32:07 -0500

    onClick on button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types';
 
 const Button = ({color, text}) => {
+    const onClick = (e) => {
+        window.localStorage.setItem("event", e.value)
+        console.log(e);
+    }
     return (
         <div>
-            <button style={{backgroundColor: color}}className="btn">{text}</button>
+            <button onClick={onClick} style={{backgroundColor: color}}className="btn">{text}</button>
 
         </div>
     )
